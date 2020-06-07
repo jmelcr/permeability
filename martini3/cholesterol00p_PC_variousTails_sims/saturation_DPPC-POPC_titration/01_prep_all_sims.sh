eda2117acd225660375f6648314735141c6d4e18
Josef Melcr <j.melcr@rug.nl>
2020-06-07 16:04:32 +0200

    Updated mdp to be standard 5 us long simulation, small fixes in prep scripts for sims with NO CHol

--- martini3/cholesterol00p_PC_variousTails_sims/saturation_DPPC-POPC_titration/01_prep_all_sims.sh
+++ martini3/cholesterol00p_PC_variousTails_sims/saturation_DPPC-POPC_titration/01_prep_all_sims.sh
@@ -16,8 +16,7 @@ do
 mkdir -p perm_particle_${perm} && cd perm_particle_${perm} || exit 8
 
 # for various level of (un)saturation
-# UNCOMMENT at the end
-for n_unsat in 0 8  #`seq 0 17 $n_lipids_tot` $n_lipids_tot 
+for n_unsat in `seq 0 17 $n_lipids_tot` $n_lipids_tot 
 do
 n_sat=`echo "$n_lipids_tot - $n_unsat" | bc`
 d=`echo "$n_unsat * 50 / $n_lipids_tot ; scale=1" | bc`
@@ -31,12 +30,15 @@ sed -e "s/_SYS_NAME_/sat-unsat_${n_sat}-${n_unsat}_d_${d}/g" \
     -e "s/_PERM_/${perm}/g" \
     ${rd}/system_template.top > system.top
 
+# First entry in the molecules list seems to require non-zero number, correcting ...
+sed -e '/DPPC           0/d' system.top  -i 
+
 # for various initial conditions - phases
 for init_conf in fluid gel
 do
 mkdir -p sim1_awh_${init_conf} && cd sim1_awh_${init_conf} || exit 11
-gmx grompp -c ${rd}/../prep_eq/conf_${init_conf}.gro  -f ${rd}/../../${mdp_fname} -n ${rd}/index.ndx -p ../system.top -maxwarn 1
-gmx mdrun -awh ${rd}/awh_ref_${init_conf}.xvg -nsteps 10000 -v
+tsp gmx grompp -c ${rd}/../prep_eq/conf_${init_conf}.gro  -f ${rd}/../../${mdp_fname} -n ${rd}/index.ndx -p ../system.top -maxwarn 1
+tsp -d -N 3 gmx mdrun -awh ${rd}/awh_ref_${init_conf}.xvg -nsteps 10000 -v
 
 cd ../
 done
